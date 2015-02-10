0f878a6aacfb988b525ba3bd120dce0a941e6141
drewzh <me@drewzh.com>
2015-02-10 01:36:24 +0000

    Misc changes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM centos:centos6
+MAINTAINER drewzh <me@drewzh.com>
 # Enable EPEL for Node.js
 RUN rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 # Install Node.js and npm
-RUN yum install -y npm
+RUN yum install -y nodejs npm
 # Bundle app source
 COPY ./dist /src
 # Expose app on port
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-    to5 = require('gulp-6to5');
+    to5 = require('gulp-6to5'),
+    notify = require('gulp-notify');
 
 gulp.task('default', function () {
     return gulp.src(['src/**/*.js'])
         .pipe(to5())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(notify({message: 'NodeFW build process complete!', onLast: true}));
 });
