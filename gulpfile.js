4a8154bc102a876df52e5d67f371d52852bd49c9
drewzh <me@drewzh.com>
2015-11-22 21:23:04 +0000

    Updated to use Node 5.x and remove babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-    babel = require('gulp-babel'),
     notify = require('gulp-notify');
 
-gulp.task('default', function () {
+gulp.task('build', function(){
     return gulp.src(['src/**/*.js'])
-        .pipe(babel())
         .pipe(gulp.dest('dist'))
         .pipe(notify({message: 'NodeFW build process complete!', onLast: true}));
 });
+
+gulp.task('default', ['build']);
