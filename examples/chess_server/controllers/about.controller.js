bdb4e4e196c5e06f71be5e29a5349de89534172b
drewzh <me@drewzh.com>
2015-12-09 14:13:35 +0000

    Removed example apps

--- examples/chess_server/controllers/about.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class AboutController {
-    index(req, res){
-        return res.send(200, 'This is the about page');
-    }
-}
-
-module.exports = AboutController;
--- examples/chess_server/controllers/index.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class IndexController {
-    index(req, res){
-        return res.send(200, 'This is the index page');
-    }
-}
-
-module.exports = IndexController;
